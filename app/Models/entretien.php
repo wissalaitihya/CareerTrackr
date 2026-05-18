84d5cd4a9e87af82c134db7ea139264d0ed2d6d0
wissalaitihya <wissalaitihya@gmail.com>
2026-05-18 16:27:02 +0100

    create models and relations

--- app/Models/entretien.php
+++ app/Models/entretien.php
@@ -4,7 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class entretien extends Model
 {
-    //
+    protected $fillable =[
+    'candidature_id',
+    'type',
+    'schedules_at',
+    'preparation_notes',
+    'result',
+    'created_at',
+    'updated_at',
+    ];
+   
+    public function candidature()
+    {
+        return $this->belongsTo(Candidature::class);
+    
+    }
 }
