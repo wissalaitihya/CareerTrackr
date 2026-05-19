0ae0fc62dab06ddd4dd0058317ad54c84e07f153
wissalaitihya <wissalaitihya@gmail.com>
2026-05-19 11:54:36 +0100

    feat(US2): add index method in CandidatureController

--- app/Http/Controllers/CandidatureController.php
+++ app/Http/Controllers/CandidatureController.php
@@ -6,5 +6,39 @@ use Illuminate\Http\Request;
 
 class CandidatureController extends Controller
 {
-    //
+   public function index()
+    {
+        $candidatures = auth()->user()->candidatures()->with('entretiens')->get();
+        return view('candidatures.index', compact('candidatures'));
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
 }
